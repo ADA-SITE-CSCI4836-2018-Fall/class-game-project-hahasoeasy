554fc8615c91fb959da10815e7f94cb2e4cdd2f6
Selin <shebipoghlu2019@ada.edu.az>
2018-11-21 08:58:25 +0400

    changed project to actual one

---
+++ Class-Game-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    float speed = 5f;
+    float jumpspeed = 10f;
+    private float movement = 0f;
+    private Rigidbody2D rigidbody;
+    public Transform groundcheckpoint;
+    public float groundcheckradius;
+    public LayerMask groundlayer;
+    private bool isTouchingGround;
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movement = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+
+        if (Input.GetButtonDown("Jump") )
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpspeed);
+        }
+    }
+}
